c9e2027f002123f43c94ca40d5fa89c346072633
Dropio12 <anass.elhallaoui@gmail.com>
2022-12-31 23:25:10 -0500

    return to normal

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
 // This source code is subject to the terms of the Mozilla Public License 2.0 at https://mozilla.org/MPL/2.0/
 // © Kokoabe
 
-//@version=5
-indicator("Mon script",overlay=true)// overlay pour le mettre sur le chart
-float b=1
-float a=1
-b:=math.max(close,open)//return la valeur la plus grande entre eux
-if b==20
-    b:=close
-if a==20
-    a:=open
-for i=1 to 3
-    b:=b+1
-while a<10
-    a:=a+1
-    if a<12
-        break
+//@version=4
+strategy("rsistratvid",overlay=true)
 
-switch
-    a==10 => b:=1// si a = 10 donc b va etre mis a 1
-    a==20 => b:=2
-    a==30 => b:=3
-f(x,y) => // fonction
-    sum=(x+y)/2
-    sum2=sum/2
-    [sum,sum2] //return sum & sum2
-    //il faudra deux variable pour contenir ces deux resultats
+// Define the input parameters
+length = 50
 
+// Calculate the momentum indicator
+fako = close - close[length]
 
-[K1,K2]= f(close[1], open[1]) // open [1] == le prix d'entrée de la dernière journée(hier)
-MOVINGAVG= ta.sma(close,200)//calculate 200 day moving average (ta= technical analysis & sma= moving average)
-RSI= ta.rsi(close,14)// 14 period rsi
-HIGHESTCLOSE= ta.highest(close,50)//highest close of the last 50 days
+// If the momentum is positive, enter a long position
+if (fako > 0)
+    strategy.entry("Long", strategy.long)
 
+// If the momentum is negative, enter a short position
 
-plot(K2,color = color.red, style=plot.style_histogram)//le style du plot(histograme,fleche,etc)
-plot(b,color=color.new(#955628,0)) //le premier chiffre est la couleur et deuxieme l'opacité
-plot(a,title = 'Allo')//pour mettre un titre
+// If the close price is below the 200-day moving average, exit the long position
+if close < sma(close, 165) and fako<-0.0163 //and close< rsi(close,50)
+    strategy.close("Long")
+
+// If the close price is below the 200-day moving average, exit the short position
+
+// Plot the indicator value
+plot(fako, color=color.rgb(113, 0, 212), linewidth=2, title="FAKO")
+
+// Plot the 200-day moving average
+plot(sma(close, 165), color=color.blue, linewidth=2, title="SMA")
+
+// Plot the RSI indicator
+plot(rsi(close, 50), color=color.green, linewidth=2, title="RSI")
+
+// Plot the 50-day moving average
+plot(sma(close, 50), color=color.orange, linewidth=2, title="SMA")
+
+// Plot the 200-day moving average
+plot(sma(close, 200), color=color.yellow, linewidth=2, title="SMA")
+
+// Plot the 200-day moving average
+plot(sma(close, 100), color=color.red, linewidth=2, title="SMA")
