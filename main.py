3ade7b72619d0ecaa7e19fd5c28538eb78972fe6
Dropio12 <anass.elhallaoui@gmail.com>
2023-01-01 23:45:42 -0500

    Second strategy using  KC, RSI and BB to try to create a 100% profitable bot. Only problem, it's not doing a lot of trades and just winning 5% each 10y or so by doing one trade.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 // © Kokoabe
 
 //@version=4
-strategy("rsistratvid",overlay=true)
+strategy("rsistratV2",overlay=true)
 
 // Define the input parameters
 length = 50
@@ -39,3 +39,59 @@ plot(sma(close, 200), color=color.yellow, linewidth=2, title="SMA")
 
 // Plot the 200-day moving average
 plot(sma(close, 100), color=color.red, linewidth=2, title="SMA")
+
+
+
+//Second strategy
+
+
+//@version=5
+strategy(shorttitle='SQZMOM_LB_Strategy', title='Squeeze Momentum Strategy [LazyBear & Kokoabe]', overlay=false)
+
+// Input parameters
+length = input(20, title='BB Length')
+mult = input(2.0, title='BB MultFactor')
+lengthKC = input(20, title='KC Length')
+multKC = input(1.5, title='KC MultFactor')
+useTrueRange = input(true, title='Use TrueRange (KC)')
+
+// Calculate BB
+source = close
+basis = ta.sma(source, length)
+dev = multKC * ta.stdev(source, length)
+upperBB = basis + dev
+lowerBB = basis - dev
+
+// Calculate KC
+ma = ta.sma(source, lengthKC)
+range_1 = useTrueRange ? ta.tr : high - low
+rangema = ta.sma(range_1, lengthKC)
+upperKC = ma + rangema * multKC
+lowerKC = ma - rangema * multKC
+
+sqzOn = lowerBB > lowerKC and upperBB < upperKC
+sqzOff = lowerBB < lowerKC and upperBB > upperKC
+noSqz = sqzOn == false and sqzOff==false
+
+val = ta.linreg(source - math.avg(math.avg(ta.highest(high, lengthKC), ta.lowest(low, lengthKC)), ta.sma(close, lengthKC)), lengthKC, 0)
+
+bcolor = val > 0 ? val > nz(val[1]) ? color.lime : color.green : val < nz(val[1]) ? color.red : color.maroon
+scolor = noSqz ? color.blue : sqzOn ? color.black : color.gray
+plot(val, color=bcolor, style=plot.style_histogram, linewidth=4)
+plot(0, color=scolor, style=plot.style_cross, linewidth=2)
+
+//rsi
+rsiLength = 50
+rsiThreshold = 70
+
+rsi = ta.rsi(close, rsiLength)
+if noSqz and val < 0 and rsi < 100 - rsiThreshold
+    strategy.entry('Short', strategy.short, stop=strategy.position_avg_price * 1.05)
+    strategy.exit('Trailing Stop', 'Short', stop=strategy.position_avg_price * 0.90)
+if noSqz and val > 0 and rsi > rsiThreshold
+    strategy.entry('Long', strategy.long, stop=strategy.position_avg_price * 0.95)
+    strategy.exit('Long Trailing Stop', 'Long', stop=strategy.position_avg_price * 1.10)
+if sqzOn
+    strategy.close('Long')
+    strategy.close('Short')
+
